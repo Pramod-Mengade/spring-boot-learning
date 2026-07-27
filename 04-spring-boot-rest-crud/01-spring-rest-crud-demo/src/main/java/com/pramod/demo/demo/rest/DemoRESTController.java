247c58f50b52d4ce48ca19aa90b926e31f89df73
Pramod <pramodmengade07@gmail.com>
2026-07-27 22:48:44 +0530

    Successfully Tested The Working Of Jackson by Converting Java POJO to JSON

--- 04-spring-boot-rest-crud/01-spring-rest-crud-demo/src/main/java/com/pramod/demo/demo/rest/DemoRESTController.java
+++ 04-spring-boot-rest-crud/01-spring-rest-crud-demo/src/main/java/com/pramod/demo/demo/rest/DemoRESTController.java
@@ -1,19 +1,31 @@
 package com.pramod.demo.demo.rest;
 
+import com.pramod.demo.demo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/test")
 public class DemoRESTController {
 
-    @GetMapping("/hello")
+    @GetMapping("/student")
 
-    public String hello(){
-        return "Hello world";
+    public List<Student> data(){
+        List <Student> list = new ArrayList<>();
+        Student s1 = new Student("Pramod" , "Mengade");
+        list.add(s1);
+        Student s2 = new Student("Prasad" ,"Mengade");
+        list.add(s2);
+        Student s3 = new Student("Pooja" ,"Mengade");
+        list.add(s3);
+        Student s4 = new Student("Arti" ,"Mengade");
+
+        return list;
     }
-
 }
